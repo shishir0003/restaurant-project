ec7352972317e967cf3d80d44acd92fe0044863c
shishir0003 <86924960+shishir0003@users.noreply.github.com>
2021-07-04 23:10:39 +0600

    Restaurant html templete

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// active nav
+let nav = document.querySelector(".navigation-wrap");
+window.onscroll = function(){
+    if(document.documentElement.scrollTop>20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
+//hide nav
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function (a){
+    a.addEventListener("click",function(){
+        navCollapse.classList.remove("show");
+    })
+})
+//counter
+
+
